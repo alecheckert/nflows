9540fadaf6e77c677a18ea5c3272651d9f33160f
Alec Heckert <alecheckert@gmail.com>
2023-10-12 12:58:25 -0400

    another test for finite_differences

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,7 @@ from nflows.utils import finite_differences
 
 
 class TestFiniteDifferences(unittest.TestCase):
-    def test_finite_differences_linear(self):
+    def test_linear(self):
         slope = 3.0
         offset = -2.0
         delta = 1e-3
@@ -18,7 +18,7 @@ class TestFiniteDifferences(unittest.TestCase):
         assert df_dX.shape == X.shape
         np.testing.assert_allclose(df_dX, slope / size, atol=1e-5, rtol=1e-5)
 
-    def test_finite_differences_quadratic(self):
+    def test_quadratic(self):
         delta = 1e-3
 
         def f(X: np.ndarray) -> float:
@@ -31,3 +31,20 @@ class TestFiniteDifferences(unittest.TestCase):
         # Analytical derivatives
         df_dX_ana = (2 / size) * X
         np.testing.assert_allclose(df_dX_num, df_dX_ana, atol=1e-5, rtol=1e-5)
+
+    def test_harmonic(self):
+        delta = 1e-4
+        a = 0.5
+        b = -2.5
+        c = 5.0
+
+        def f(X: np.ndarray) -> float:
+            return (b * np.cos(a * X) + c).sum()
+
+        X = np.random.normal(size=(10, 3, 5))
+        size = np.prod(X.shape)
+        # Numerical derivatives
+        df_dX_num = finite_differences(f, X, delta=delta)
+        # Analytical derivatives
+        df_dX_ana = -a * b * np.sin(a * X)
+        np.testing.assert_allclose(df_dX_num, df_dX_ana, atol=1e-3, rtol=1e-2)
